be78798facfd635786902a609160a05142288b85
princessmx <caomeixuan@gmail.com>
2013-09-12 11:54:58 +0800

    Create Remove Nth Node From End of List.cc

---
+++ Remove Nth Node From End of List.cc
@@ -0,0 +1,30 @@
+// Small: 16ms; Large: 36ms
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode *fast = head, *slow = head, *prev = NULL;
+        for (int i = 1; i < n; i++) {
+            fast = fast->next;
+        }
+        while (fast->next != NULL) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        if (prev) {
+            prev->next = slow->next;
+        } else {
+            head = slow->next;
+        }
+        delete slow;
+        return head;
+    }
+};
