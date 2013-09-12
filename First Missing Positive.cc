1d350c4a14f6e9076b7d4f35b8f6a4311ad114a4
princessmx <caomeixuan@gmail.com>
2013-09-12 11:03:42 +0800

    Create First Missing Positive.cc

---
+++ First Missing Positive.cc
@@ -0,0 +1,19 @@
+// Small: 4ms; Large: 12ms
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        for (int i = 0; i < n;) {
+            if (A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i]) {
+                swap(A[i], A[A[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
