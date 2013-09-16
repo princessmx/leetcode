e17ae138430ca16bca3fef43bc12fa2a621775ac
princessmx <caomeixuan@gmail.com>
2013-09-16 09:08:45 +0800

    Update Two Sum.cc

--- Two Sum.cc
+++ Two Sum.cc
@@ -1,57 +1,37 @@
-// Small: 4ms; Large: 16ms
+// Small: 4ms; Large: 12ms
 class Solution {
- public:
-  vector<int> twoSum(vector<int> &numbers, int target) {
-    vector<Number> nums;
-    for (int i = 0; i < numbers.size(); i++) {
-      nums.push_back(Number(numbers[i], i + 1));
-    }
-    sort(nums.begin(), nums.end());
-    vector<int> res(2);
-    for (int i1 = 0; i1 < nums.size(); i1++) {
-      int i2 = bsearch(nums, target - nums[i1].num, i1);
-      if (i2 >= 0) {
-        if (nums[i1].index < nums[i2].index) {
-          res[0] = nums[i1].index;
-          res[1] = nums[i2].index;
-        } else {
-          res[0] = nums[i2].index;
-          res[1] = nums[i1].index;
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<Number> nums;
+        for (int i = 0; i < numbers.size(); i++) {
+            nums.push_back(Number(numbers[i], i + 1));
         }
-        break;
-      }
-    }
-    return res;
-  }
- private:
-  struct Number {
-    Number(int _num, int _index)
-        : num(_num), index(_index) {}
-
-    bool operator<(const Number &other) const {
-      return num < other.num || (num == other.num && index < other.index);
-    }
-
-    int num;
-    int index;
-  };
-
-  int bsearch(const vector<Number>& nums, int num, int black_index) {
-    int l = 0, r = nums.size() - 1;
-    while (l <= r) {
-      int mid = (l + r) / 2;
-      if (nums[mid].num < num) {
-        l = mid + 1;
-      } else if (nums[mid].num > num) {
-        r = mid - 1;
-      } else {
-        if (mid == black_index && nums[mid + 1].num == num) {
-          return mid + 1;
-        } else {
-          return mid;
+        sort(nums.begin(), nums.end());
+        vector<int> res(2);
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int sum = nums[l].val + nums[r].val;
+            if (sum > target) {
+                r--;
+            } else if (sum < target) {
+                l++;
+            } else {
+                res[0] = min(nums[l].index, nums[r].index);
+                res[1] = max(nums[l].index, nums[r].index);
+                break;
+            }
         }
-      }
+        return res;
     }
-    return -1;
-  }
+private:
+    struct Number {
+        Number(int _val, int _index) : val(_val), index(_index) {}
+        
+        bool operator<(const Number &other) const {
+            return val < other.val || (val == other.val && index < other.index);
+        }
+        
+        int val;
+        int index;
+    };
 };
